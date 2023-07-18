50fbd885162414a5f1df5d1726a7b4aaad8048a7
Markus Heiliger <markus.heiliger@dash-two.de>
2023-07-18 22:03:04 +0200

    ...

--- resources/scripts/initMachine.sh
+++ resources/scripts/initMachine.sh
@@ -15,4 +15,5 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update -y && sudo apt-get upgrade -y 
 
 # install commonly used packages
-sudo apt-get install -y apt-utils apt-transport-https coreutils net-tools
+sudo apt-get install -y apt-utils apt-transport-https coreutils
+
