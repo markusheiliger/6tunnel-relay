9fee2010e34381467299c4f157ecd41953b92268
Markus Heiliger <markus.heiliger@dash-two.de>
2023-07-19 10:40:55 +0200

    ...

--- resources/scripts/setup6Tunnel.sh
+++ resources/scripts/setup6Tunnel.sh
@@ -13,16 +13,16 @@ timeout 10;
 EOF
 
 # patch network configuration
-cat << EOF | sudo tee /etc/cloud/cloud.config.d/91-azure-network.cfg
+cat << EOF | sudo tee /etc/cloud/cloud.cfg.d/91-azure-network.cfg
 
 network:
   version: 2
   ethernets:
-  eth0:
-    dhcp4: true
-    dhcp6: true
-    match:
-      driver: hv_netvsc
-    set-name: eth0
+    eth0:
+      dhcp4: true
+      dhcp6: true
+      match:
+        driver: hv_netvsc
+      set-name: eth0
 
 EOF
