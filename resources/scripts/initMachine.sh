df52e2b3f8c9f1cb30ada8421496b603a5038d22
Markus Heiliger <markus.heiliger@dash-two.de>
2023-07-19 10:04:14 +0200

    ...

--- resources/scripts/initMachine.sh
+++ resources/scripts/initMachine.sh
@@ -11,6 +11,11 @@ export DEBIAN_FRONTEND=noninteractive
 [ -f '/etc/needrestart/needrestart.conf' ] \
 	&& sed -i 's/#$nrconf{restart}.*/$nrconf{restart} = '"'"'l'"'"';/g' /etc/needrestart/needrestart.conf
 
+# register Microsoft package feed
+curl -sSL https://packages.microsoft.com/config/$(lsb_release -si)/$(lsb_release -sr)/prod.list | sudo tee /etc/apt/sources.list.d/microsoft-prod.list
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
+sudo dpkg -i packages-microsoft-prod.deb && rm packages-microsoft-prod.deb
+
 # update and upgrade packages
 sudo apt update -y && sudo apt upgrade -y 
 
